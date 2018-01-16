6a0e4c7bd87b8887256de75464ea643a32b506dc
lanthree <lanthree@qq.com>
2018-01-16 14:17:36 +0800

    what happend

--- src/log.h
+++ src/log.h
@@ -1,10 +1,23 @@
 #ifndef __LOG_H__
 #define __LOG_H__
 
+#include <string>
+
+enum class LOG_LEVEL {
+	ERROR, WARN, INFO, DEBUG
+};
+
 class Log
 {
+public:
+	Log(const std::string& pathname, long long maxsize_eachfile);
 
-}
+	Print(const std::string& file
 
+private:
+	std::string pathname_;
+	long long maxsize_eachfile_;
+
+};
 
 #endif
